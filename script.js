d7bda78f34330129250c657ebb93ed2ee75517de
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-08-17 21:06:03 +0530

    add properties of cactus and their images and create a update function for animationframe

--- script.js
+++ script.js
@@ -19,6 +19,22 @@ let dino = {
     height: dinoHeight
 };
 
+// cactus properties
+let cactusArr = [];
+let cactus1Width = 34;
+let cactus2Width = 69;
+let cactus3Width = 102;
+
+let cactusHeight = 70;
+let cactusX = 700;
+let cactusY = boardHeight - cactusHeight;
+
+let cactusImg1;
+let cactusImg2;
+let cactusImg3;
+
+
+// onloading function
 window.onload = function () {
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -33,5 +49,21 @@ window.onload = function () {
         context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
     }
 
+    // adding cactus imgs
+    cactusImg1 = new Image();
+    cactusImg1.src = ".img/cactus1.png";
+
+    cactusImg2 = new Image();
+    cactusImg2.src = ".img/cactus2.png";
+
+    cactusImg3 = new Image();
+    cactusImg3.src = ".img/cactus3.png";
+
+    requestAnimationFrame(update);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
 }
 
