321c1c3f5b9706f34d002c629673c12e05773ade
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-08-17 20:44:28 +0530

    added properties of the board and function to load dino on windows load

---
+++ script.js
@@ -0,0 +1,37 @@
+// board properties
+let board;
+let boardWidth = 750;
+let boardHeight = 250;
+let context;
+
+// dinosaur properties
+
+let dinoWidth = 88;
+let dinoHeight = 90;
+let dinoX = 58;
+let dinoY = boardHeight - dinoHeight;
+let dinoImg;
+
+let dino = {
+    x: dinoX,
+    y: dinoY,
+    width: dinoWidth,
+    height: dinoHeight
+};
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    context = board.getContext("2d");
+
+    // adding dinosour image
+    dinoImg = new Image();
+    dinoImg.src = "./img/dino.png";
+    dinoImg.onload = function () {
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+    }
+
+}
+
